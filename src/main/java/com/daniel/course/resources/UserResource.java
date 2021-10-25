cbb7d9422e04d2c27ce9b757551d62c2ea0cf0fd
Daniel Rocha <danrochamaia@gmail.com>
2021-10-24 23:26:12 -0300

    Entidade User e seu resource

---
+++ src/main/java/com/daniel/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.daniel.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daniel.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Daniel", "daniel@gmail.com", "85981419682", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+
+}
