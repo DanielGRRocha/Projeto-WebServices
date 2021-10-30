1fcd0d123b3d20db51f6ff7246ef9e0cdeae189a
Daniel Rocha <danrochamaia@gmail.com>
2021-10-30 13:26:38 -0300

    Entidade Payment, associação um para um

--- src/main/java/com/daniel/course/config/TestConfig.java
+++ src/main/java/com/daniel/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.daniel.course.entities.Category;
 import com.daniel.course.entities.Order;
 import com.daniel.course.entities.OrderItem;
+import com.daniel.course.entities.Payment;
 import com.daniel.course.entities.Product;
 import com.daniel.course.entities.User;
 import com.daniel.course.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
