1295b5f7d6b91e0bba0d8e7f7ac4541908a2dffc
Daniel Rocha <danrochamaia@gmail.com>
2021-10-31 18:07:10 -0300

    Tratamento de exceção - delete

--- src/main/java/com/daniel/course/services/UserService.java
+++ src/main/java/com/daniel/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.daniel.course.entities.User;
 import com.daniel.course.repositories.UserRepository;
+import com.daniel.course.services.exceptions.DatabaseException;
 import com.daniel.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,8 +33,17 @@ public class UserService {
 		return repository.save(obj);
 	}
 	
+	//tratar exceções
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {//excluir user que não existe
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {//excluir user que tem pedido
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
