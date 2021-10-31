ef42979b4544b479e7330727749855ba8a4b5f34
Daniel Rocha <danrochamaia@gmail.com>
2021-10-31 15:22:39 -0300

    Deleção de User

--- src/main/java/com/daniel/course/resources/UserResource.java
+++ src/main/java/com/daniel/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.daniel.course.entities.User;
 import com.daniel.course.services.UserService;
-
+//end points (padrão Rest)
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
@@ -42,5 +43,13 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
+	
+	@DeleteMapping(value = "/{id}") //anotation Spring Boot
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 
 }
